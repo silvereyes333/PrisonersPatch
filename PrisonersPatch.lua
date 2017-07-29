b0f44cdf1d782f2aac516695ebe3b13e47dd7947
silvereyes <silvereyes333@gmail.com>
2017-07-28 21:50:30 -0500

    Version 1.1.2
    - Update 15 Horns of the Reach compatibility

--- PrisonersPatch.lua
+++ PrisonersPatch.lua
@@ -4,7 +4,7 @@
 local PrisonersPatch = {
     name = "PrisonersPatch",
     title = "Prisoner's Patch",
-    version = "1.1.1",
+    version = "1.1.2",
     author = "|c99CCEFsilvereyes|r,",
     debugMode = false,
 }
